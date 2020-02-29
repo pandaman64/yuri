b4bf744e1f5f2a1dbf19e0dc64f9b111e5e19292
pandaman64 <kointosudesuyo@infoseek.jp>
2020-02-29 19:17:09 +0900

    basic lexer

--- yuri-lexer/src/lib.rs
+++ yuri-lexer/src/lib.rs
@@ -1,8 +1,11 @@
-pub struct Span {
-    pub line: usize,
-    pub column: usize,
-    pub len: usize,
-}
+use nom::branch::alt;
+use nom::character::complete::{alpha1, alphanumeric0, multispace0};
+use nom::combinator::recognize;
+use nom::sequence::{delimited, tuple};
+use nom::IResult;
+
+type Span<'a> = nom_locate::LocatedSpan<&'a str>;
+
 pub enum TokenKind {
     Ident,
     ParenOpen,
@@ -17,23 +20,60 @@ pub enum TokenKind {
     Slash,
 }
 
-pub struct Token<'s> {
+pub struct Token<'a> {
     pub kind: TokenKind,
-    pub span: Span,
-    pub token: &'s str,
+    pub span: Span<'a>,
 }
 
-fn first_token(input: &str) -> (usize, Token) {
-    todo!()
-}
-
-pub fn tokenize(mut input: &str) -> impl Iterator<Item = Token> + '_ {
-    std::iter::from_fn(move || {
-        if input.is_empty() {
-            return None;
+macro_rules! token_symbol {
+    ($name:ident, $tag:expr, $kind:expr) => {
+        fn $name(s: Span) -> IResult<Span, Token> {
+            nom::bytes::complete::tag($tag)(s)
+                .map(|(s, span)| {
+                    (s, Token {
+                        kind: $kind,
+                        span,
+                    })
+                })
         }
-        let (pos, token) = first_token(input);
-        input = &input[pos + token.len..];
-        Some(token)
-    })
+    };
+}
+
+token_symbol!(token_paren_open, "(", TokenKind::ParenOpen);
+token_symbol!(token_paren_close, ")", TokenKind::ParenClose);
+token_symbol!(token_brace_open, "{", TokenKind::BraceOpen);
+token_symbol!(token_brace_close, "}", TokenKind::BraceClose);
+token_symbol!(token_colon, ":", TokenKind::Colon);
+token_symbol!(token_equal, "=", TokenKind::Equal);
+token_symbol!(token_plus, "+", TokenKind::Plus);
+token_symbol!(token_minus, "-", TokenKind::Minus);
+token_symbol!(token_star, "*", TokenKind::Star);
+token_symbol!(token_slash, "/", TokenKind::Slash);
+
+fn token_ident(s: Span) -> IResult<Span, Token> {
+    // alphabetic followed by alphanumerics
+    recognize(tuple((alpha1, alphanumeric0)))(s)
+        .map(|(s, span)| {
+            (s, Token {
+                kind: TokenKind::Ident,
+                span,
+            })
+        })
+}
+
+pub fn next_token(s: Span) -> IResult<Span, Token> {
+    let alt = alt((
+        token_paren_open,
+        token_paren_close,
+        token_brace_open,
+        token_brace_close,
+        token_colon,
+        token_equal,
+        token_plus,
+        token_minus,
+        token_star,
+        token_slash,
+        token_ident,
+    ));
+    delimited(multispace0, alt, multispace0)(s)
 }
